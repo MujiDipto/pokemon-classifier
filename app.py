5938c215b021ceb487a3167aadf212e96f928e12
MujiDipto <m.dipto@uqconnect.edu.au>
2021-07-14 00:31:03 +1000

    updated marowalk & reduced dependencies

--- app.py
+++ app.py
@@ -4,19 +4,8 @@ from werkzeug.datastructures import  FileStorage
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, Activation
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import categorical_crossentropy
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Model
-from tensorflow.keras.applications import imagenet_utils
-from sklearn.metrics import confusion_matrix
-import itertools
 import os
-import shutil
-import random
-import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 import cv2
 from PIL import Image
